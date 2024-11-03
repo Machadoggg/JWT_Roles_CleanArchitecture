a03f52a945fdd839387525b78774780afb1696fa
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-03 00:48:01 -0500

    Add endpoint Login

---
+++ JWT_Roles_CleanArchitecture.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using JWT_Roles_CleanArchitecture.Application.Models;
+using JWT_Roles_CleanArchitecture.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Roles_CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] AuthRequest request)
+        {
+            var token = _authService.Authenticate(request);
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(new { token });
+        }
+
+
+        
+
+    }
+}
