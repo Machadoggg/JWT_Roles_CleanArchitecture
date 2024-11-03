4c9c98738dc754324a671e6275b53193744aa020
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-02 23:15:09 -0500

    Add autentication interfaz

---
+++ JWT_Roles_CleanArchitecture.Core/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using JWT_Roles_CleanArchitecture.Core.Entities;
+
+namespace JWT_Roles_CleanArchitecture.Core.Interfaces
+{
+    public interface IAuthService
+    {
+        string GenerateToken(User user);
+        bool ValidateUser(string username, string password);
+    }
+}
