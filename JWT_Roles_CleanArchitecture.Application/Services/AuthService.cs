2c1ab378d409a8d52759be9dae5c2b5181f4972a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-02 23:45:59 -0500

    Autentication use case AuthService

---
+++ JWT_Roles_CleanArchitecture.Application/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using JWT_Roles_CleanArchitecture.Application.Models;
+using JWT_Roles_CleanArchitecture.Core.Entities;
+using JWT_Roles_CleanArchitecture.Core.Interfaces;
+
+namespace JWT_Roles_CleanArchitecture.Application.Services
+{
+    public class AuthService
+    {
+        private readonly IAuthService _authService;
+
+        public AuthService(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        public string Authenticate(AuthRequest request)
+        {
+            if (_authService.ValidateUser(request.Username, request.Password)) 
+            {
+                var user = new User { Username = request.Username, Role = request.Username == "admin" ? "Admin" : "User" };
+                return _authService.GenerateToken(user);
+            }
+            return null!;
+        }
+    }
+}
